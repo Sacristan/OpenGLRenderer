928f5b5dcdb9478d68eb05fda7290cead13b62c5
Sacristan <girts.kesteris@gmail.com>
2019-03-03 00:34:33 +0200

    add vertex array object

--- PD_OpenGL/PD_OpenGL/Application.cpp
+++ PD_OpenGL/PD_OpenGL/Application.cpp
@@ -11,7 +11,7 @@
 	x;\
 	ASSERT(GLLogCall(#x, __FILE__, __LINE__))
 
-static void GLClearErrors() 
+static void GLClearErrors()
 {
 	while (glGetError() != GL_NO_ERROR);
 }
@@ -20,7 +20,7 @@ static bool GLLogCall(const char* function, const char* file, int line)
 {
 	while (GLenum error = glGetError())
 	{
-		std::cout <<"OpenGL Error: ("<< error << ")" 
+		std::cout << "OpenGL Error: (" << error << ")"
 			<< function << " "
 			<< file << ":"
 			<< line << ":"
@@ -117,6 +117,10 @@ int main(void)
 	if (!glfwInit())
 		return -1;
 
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
 	window = glfwCreateWindow(640, 480, "...OpenGL Magick...", NULL, NULL);
 	if (!window)
 	{
@@ -146,10 +150,14 @@ int main(void)
 		2, 3, 0
 	};
 
-	unsigned int bufferId;
-	GLCall(glGenBuffers(1, &bufferId));
-	GLCall(glBindBuffer(GL_ARRAY_BUFFER, bufferId));
-	GLCall(glBufferData(GL_ARRAY_BUFFER, 6 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
+	unsigned int vao;
+	GLCall(glGenVertexArrays(1, &vao));
+	GLCall(glBindVertexArray(vao));
+
+	unsigned int vertexBuffer;
+	GLCall(glGenBuffers(1, &vertexBuffer));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, vertexBuffer));
+	GLCall(glBufferData(GL_ARRAY_BUFFER, 4 * 2 * sizeof(float), positions, GL_STATIC_DRAW));
 
 	GLCall(glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(float) * 2, 0));
 	GLCall(glEnableVertexAttribArray(0));
@@ -167,14 +175,24 @@ int main(void)
 	ASSERT(uniformLocation != -1);
 	//GLCall(glUniform4f(uniformLocation, 1.0f, 1.0f, 1.0f, 1.0f));
 
+	GLCall(glBindVertexArray(0));
+	GLCall(glUseProgram(0));
+	GLCall(glBindBuffer(GL_ARRAY_BUFFER, 0));
+	GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0));
+
 	float r = 0.0f;
 	float increment = 0.05f;
 
 	while (!glfwWindowShouldClose(window))
 	{
 		glClear(GL_COLOR_BUFFER_BIT);
-	
+
+		GLCall(glUseProgram(shader));
 		GLCall(glUniform4f(uniformLocation, r, 0.25f, 1.0f, 1.0f));
+		
+		GLCall(glBindVertexArray(vao));
+		GLCall(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo));
+
 		GLCall(glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, nullptr));
 
 		if (r > 1.0f) increment *= -1;
