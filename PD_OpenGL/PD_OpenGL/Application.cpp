d8b389c5ed5fac8cde9b31ca34aca254a98090fb
Sacristan <girts.kesteris@gmail.com>
2019-03-05 14:43:54 +0200

    add IMGUI

--- PD_OpenGL/PD_OpenGL/Application.cpp
+++ PD_OpenGL/PD_OpenGL/Application.cpp
@@ -4,6 +4,10 @@
 	* Support TEXCOORD
 */
 
+#include "imgui/imgui.h";
+#include "imgui/imgui_impl_glfw.h";
+#include "imgui/imgui_impl_opengl3.h";
+
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
 
@@ -148,10 +152,50 @@ int main(void)
 
 		Renderer renderer;
 
+		IMGUI_CHECKVERSION();
+		
+		ImGui::CreateContext();
+		//// Setup Dear ImGui style
+		ImGui::StyleColorsDark();
+
+		//// Setup Platform/Renderer bindings
+		ImGui_ImplGlfw_InitForOpenGL(window, true);
+		ImGui_ImplOpenGL3_Init("#version 130");
+
+		bool show_demo_window = true;
+		bool show_another_window = false;
+		ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+
 		while (!glfwWindowShouldClose(window))
 		{
 			float deltaTime = clock() - lastFrameTime;
 			renderer.Clear();
+
+			ImGui_ImplOpenGL3_NewFrame();
+			ImGui_ImplGlfw_NewFrame();
+			ImGui::NewFrame();
+
+			{
+				static float f = 0.0f;
+				static int counter = 0;
+
+				ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+				ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+				ImGui::Checkbox("Demo Window", &show_demo_window);      // Edit bools storing our window open/close state
+				ImGui::Checkbox("Another Window", &show_another_window);
+
+				ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+				ImGui::ColorEdit3("clear color", (float*)&clear_color); // Edit 3 floats representing a color
+
+				if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+					counter++;
+				ImGui::SameLine();
+				ImGui::Text("counter = %d", counter);
+
+				ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / ImGui::GetIO().Framerate, ImGui::GetIO().Framerate);
+				ImGui::End();
+			}
 			
 			//float fov = PingPongValue(45.0f, 120.0f, deltaTime());
 			fovPingPong.step();
@@ -183,12 +227,19 @@ int main(void)
 			r += increment;
 			//r = sin(clock());
 
-			lastFrameTime = clock();
+			ImGui::Render();
+			ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
 
+
+			lastFrameTime = clock();
 			glfwSwapBuffers(window);
 			glfwPollEvents();
 		}
 	}
+
+	ImGui_ImplOpenGL3_Shutdown();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
 	glfwTerminate();
 	return 0;
